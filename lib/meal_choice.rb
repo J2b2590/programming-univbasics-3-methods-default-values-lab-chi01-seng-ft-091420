46a68d4d72c63027a9089a0bcbe0f17c9c949206
Jeffrey Bolling <j2b2590@gmail.com>
2020-08-25 17:26:50 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,7 @@
 
 def meal_choice (veg1, veg2, protein = "tofu")
   food_comment = "What a nutritious meal!"
-  food_comment
+  puts food_comment
   return "A plate of #{protein} with #{veg1} and #{veg2}."
 
 end
