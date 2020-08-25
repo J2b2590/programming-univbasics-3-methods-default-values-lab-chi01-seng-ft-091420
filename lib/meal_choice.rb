5bdf1d31b10ff31de6a39299dcba350eb525cd3f
Jeffrey Bolling <j2b2590@gmail.com>
2020-08-25 17:35:52 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -7,9 +7,9 @@ def meal_choice (veg1, veg2, protein = "tofu")
 
   order =  "A plate of #{protein} with #{veg1} and #{veg2}."
 
-  puts order
+  order
 
 end
 
-meal_choice('carrots', 'string beans', 'lentils')
-meal_choice('carrots', 'string beans')
+puts meal_choice('carrots', 'string beans', 'lentils')
+puts meal_choice('carrots', 'string beans')
